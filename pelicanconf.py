7206c408b86ff6a9ce6b9c47c73c4ad19844ea71
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2016-06-03 09:56:32 +0200

    Create feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,11 +19,12 @@ DEFAULT_LANG = u'en'
 DEFAULT_CATEGORY = 'tech'
 
 # Feed generation is usually not desired when developing
-# FEED_ALL_ATOM = None
-# CATEGORY_FEED_ATOM = None
-# TRANSLATION_FEED_ATOM = None
-# AUTHOR_FEED_ATOM = None
-# AUTHOR_FEED_RSS = None
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = 'feeds/%s.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/%s.atom.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss'
 
 DISPLAY_PAGES_ON_MENU = True
 
