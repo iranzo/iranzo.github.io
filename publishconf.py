35458ada12618b17470770174021ea749f919de1
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-12-07 23:48:34 +0100

    Update configuration

--- publishconf.py
+++ publishconf.py
@@ -13,12 +13,4 @@ from pelicanconf import *
 SITEURL = 'https://iranzo.github.io/'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-
 DELETE_OUTPUT_DIRECTORY = False
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
