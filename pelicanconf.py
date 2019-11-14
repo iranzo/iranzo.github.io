0c5ea5e47dc7d08e4605dd856f99dcfc45a63a8e
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-11-14 22:28:48 +0100

    Update .IT user

--- pelicanconf.py
+++ pelicanconf.py
@@ -51,7 +51,7 @@ AMAZON_BESTAZON = """var BestAzon_Configuration = {
 "Amzn_AfiliateID_DE": "redken02-21",
 "Amzn_AfiliateID_FR": "redken0c-21",
 "Amzn_AfiliateID_ES": "redken-21",
-"Amzn_AfiliateID_IT": "redken0d-21",
+"Amzn_AfiliateID_IT": "redken018-21",
 "Amzn_AfiliateID_JP": "",
 "Amzn_AfiliateID_IN": "",
 "Amzn_AfiliateID_CN": "",
