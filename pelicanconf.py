09de74488fe766e073befabfa5dbd1e039a2efb9
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-01-20 20:16:23 +0100

    Update tag

--- pelicanconf.py
+++ pelicanconf.py
@@ -54,7 +54,7 @@ AMAZON_BESTAZON = """var BestAzon_Configuration = {
 "Amzn_AfiliateID_DE": "redken02-21",
 "Amzn_AfiliateID_FR": "redken0c-21",
 "Amzn_AfiliateID_ES": "redken-21",
-"Amzn_AfiliateID_IT": "redken015-21",
+"Amzn_AfiliateID_IT": "redken0ac-21",
 "Amzn_AfiliateID_JP": "",
 "Amzn_AfiliateID_IN": "",
 "Amzn_AfiliateID_CN": "",
