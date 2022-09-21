3060f491034d5c49c72e7830e7350c2293b3cdbe
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2022-09-21 11:47:31 +0200

    Adjust formatting

--- static/js/load-photoswipe.js
+++ static/js/load-photoswipe.js
@@ -6,7 +6,7 @@ Documentation and licence at https://github.com/liwenyip/hugo-easy-gallery/
 /* Show an alert if this js file has been loaded twice */
 if (window.loadphotoswipejs) {
   window.alert(
-    "You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6"
+    "You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6",
   );
 }
 var loadphotoswipejs = 1;
